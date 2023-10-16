6d3b5b4e84b29911192fd176692d71c90f7d6f40
Diego Rojas <ale.rgdap@gmail.com>
2023-10-15 19:58:47 -0600

    second commit

--- newyear/views.py
+++ newyear/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html",{
+        "today": now.date     
+    })
+
