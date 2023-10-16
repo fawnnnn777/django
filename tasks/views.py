97e7a631d0e179fd7b28130038c1da6c119d69d7
Diego Rojas <ale.rgdap@gmail.com>
2023-10-15 20:00:27 -0600

    second commit

---
+++ tasks/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django import forms
+
+# Create your views here.
+tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+def index(request):
+    return render(request, "tasks/index.html",{
+        "tasks": tasks
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html",{
+                "form": form
+            })
+    return render(request, "tasks/add.html",{
+        "form": NewTaskForm()
+    })
